d83100895994a7bf9fd42a9dc69719eff90b68de
Alejandro <alejotoro.o@gmail.com>
2024-01-30 14:08:09 -0500

    Added diff_drive_sim

---
+++ src/diff_drive_sim/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+package_name = 'diff_drive_sim'
+data_files = []
+data_files.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
+data_files.append(('share/' + package_name + '/launch', ['launch/robot_launch.py']))
+data_files.append(('share/' + package_name + '/worlds', ['worlds/diff_drive.wbt']))
+data_files.append(('share/' + package_name + '/resource', ['resource/robot.urdf']))
+data_files.append(('share/' + package_name, ['package.xml']))
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=data_files,
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='alejandro',
+    maintainer_email='alejotoro.o@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'robot_driver = diff_drive_sim.webots_robot_driver:main',
+        ],
+    },
+)
